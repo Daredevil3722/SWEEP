e332336edd27078efc7ffa22947e8929712f2d0b
Tanishq <79592237+Daredevil3722@users.noreply.github.com>
2021-09-12 20:06:59 +0530

    Add files via upload

---
+++ InitServer.sql
@@ -0,0 +1,14 @@
+CREATE USER 'admin_SWEEP'@'localhost' IDENTIFIED BY 'admin@password@SWEEP';
+CREATE DATABASE SWEEP;
+GRANT ALL PRIVILEGES ON SWEEP.* TO 'admin_SWEEP'@'localhost';
+FLUSH PRIVILEGES;
+
+
+USE SWEEP;
+CREATE TABLE login_details(
+    username VARCHAR(50),
+    password VARCHAR(100),
+    email_or_fcbk VARCHAR(100),
+    phone_number VARCHAR(10),
+    PRIMARY KEY(username)
+);
