a58982779904c1935f34b46c8294e9fe253103dd
Tanishq <daredevil3722@gmail.com>
2022-03-16 21:50:26 +0530

    Database table changes

--- InitServer.sql
+++ InitServer.sql
@@ -13,3 +13,29 @@ CREATE TABLE login_details(
     phone_number VARCHAR(10),
     PRIMARY KEY(username)
 );
+
+CREATE TABLE net_banking(
+    username VARCHAR(50),
+    cost INT,
+    account_number INT,
+    CIF_number INT,
+    branch_code INT,
+    flat_number INT,
+    PRIMARY KEY(username)
+);
+
+CREATE TABLE upi(
+    username VARCHAR(50) PRIMARY KEY,
+    cost INT,
+    UPI_ID INT,
+    flat_number INT,
+    PRIMARY KEY(username)
+);
+
+CREATE TABLE paytm(
+    username VARCHAR(50),
+    cost INT,
+    paytm_ID INT,
+    flat_number INT,
+    PRIMARY KEY(username)
+);
